da7922d7c89481fdfc60b4dfbb4abe8a84219c88
vsm2004 <128446657+vsm2004@users.noreply.github.com>
2024-09-22 19:47:03 +0530

    Update and rename DA27.cpp to floyd's algorithm.cpp

---
+++ floyd's algorithm.cpp
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <limits.h>
+
+
+void floydWarshall(int graph[V][V]) {
+    int dist[V][V];
+
+    for (int i = 0; i < V; i++) {
+        for (int j = 0; j < V; j++) {
+            dist[i][j] = graph[i][j];
+        }
+    }
+
+    // Update distances
+    for (int k = 0; k < V; k++) {
+        for (int i = 0; i < V; i++) {
+            for (int j = 0; j < V; j++) {
+                if (dist[i][k] + dist[k][j] < dist[i][j]) {
+                    dist[i][j] = dist[i][k] + dist[k][j];
+                }
+            }
+        }
+    }
+    printf("Shortest distances between every pair of vertices:\n");
+    for (int i = 0; i < V; i++) {
+        for (int j = 0; j < V; j++) {
+            if (dist[i][j] == INT_MAX) {
+                printf("INF\t");
+            } else {
+                printf("%d\t", dist[i][j]);
+            }
+        }
+        printf("\n");
+    }
+}
+
+int main() {
+    int graph[V][V] = {
+        {0, 3, INT_MAX, 7},
+        {8, 0, 2, INT_MAX},
+        {5, INT_MAX, 0, 1},
+        {2, INT_MAX, INT_MAX, 0}
+    };
+
+    floydWarshall(graph);
+    return 0;
+}
